d7ec2fb8eed62d429c5f996c9f725cb929a93fc3
RRLWork <114157317+RRLWork@users.noreply.github.com>
2022-12-14 11:42:10 +0100

    first version

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+const {connect} = require('./src/utils/database');
+const boardgamesRoutes = require('./src/api/routes/boardgames.routes');
+const userRouter = require('./src/api/routes/users.routes');
+const PORT = process.env.PORT || 3000;
+dotenv.config();
+
+const app = express();
+connect();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }))
+
+app.use('/boardgames', boardgamesRoutes);
+
+app.use('/users', userRouter);
+
+app.listen(PORT, () => console.log(`Listening on port: http://localhost:${PORT}`));
